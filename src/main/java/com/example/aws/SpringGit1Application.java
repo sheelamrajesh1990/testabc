a37db6147425449ca1c83b805c7a39b3384a6147
Rajesh <sheelamrajesh0409@gmail.com>
2023-04-13 10:57:43 +0530

    first commit

---
+++ src/main/java/com/example/aws/SpringGit1Application.java
@@ -0,0 +1,13 @@
+package com.example.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGit1Application.class, args);
+	}
+
+}
